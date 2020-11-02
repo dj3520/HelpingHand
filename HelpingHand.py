68ec8b329d319d4b6ea083647ee4d474271c7480
dj3520 <dj3520@gmail.com>
2020-11-02 01:42:15 -0500

    Add text for each ghost type. Subject to change. Bottom grid no longer changes size. Text alignment is consistant after de-selecting evidence.

--- HelpingHand.py
+++ HelpingHand.py
@@ -16,29 +16,31 @@ display_texts = {
 # Bitmap: EMF5, Box, Prints, Orbs, Writing, Temps
 
 possibilities = {
-  "Spirit": "011010",
-  "Shade": "100110",
-  "Poltergeist": "011100",
-  "Jinn": "110100",
-  "Mare": "010101",
-  "Phantom": "100101",
-  "Wraith": "011001",
-  "Banshee": "101001",
-  "Revenant": "101010",
-  "Yurei": "000111",
-  "Oni": "110010",
-  "Demon": "010011",
+  "Spirit": ["011010", "Can be delayed with smudge sticks."],
+  "Shade": ["100110", "Shy, hunt lone players. Sticking together prevents death but also activity."],
+  "Poltergeist": ["011100", 'Moves objects, considered "noisy", useless in a room with no items.'],
+  "Jinn": ["110100", "Territorial, only attack when threatened. High speed travel unless power is cut."],
+  "Mare": ["010101", "Powerful in the dark. Will want to turn off lights, or even power."],
+  "Phantom": ["100101", "Slow. When viewed directly will take large amount of sanity. Taking its picture will make it disappear."],
+  "Wraith": ["011001", "Relentless hunter. Can fly and go through walls. Salt is toxic and will prevent attacks."],
+  "Banshee": ["101001", "Focuses on one player at a time. Crucifixes have a larger effective range."],
+  "Revenant": ["101010", "Faster while hunting, slower while players are hiding."],
+  "Yurei": ["000111", "Strong effect on sanity. Weak to smudge sticks."],
+  "Oni": ["110010", "Moves quicker while players are near. Means more activity but also more dangerous."],
+  "Demon": ["010011", "Highly aggressive. Crucifix is recommended. No penalty to sanity when using Ouiji board and it cooperates."],
 }
 
 signs = tk.LabelFrame(rootwin, text="Evidence")
 signs.grid(row=0)
 type_list = tk.LabelFrame(rootwin, text="Possibilities")
 label_dictionary = {}
-for k in possibilities.keys():
-  label_dictionary[k] = tk.Label(type_list, text=k)
-  label_dictionary[k].grid(row=len(label_dictionary) - 1)
+for k, v in possibilities.items():
+  label_dictionary[k] = [None, None]
+  label_dictionary[k][0] = tk.Label(type_list, text=k)
+  label_dictionary[k][0].grid(row=len(label_dictionary) - 1, column=0, sticky=tk.W)
+  label_dictionary[k][1] = tk.Label(type_list, text=v[1])
+  label_dictionary[k][1].grid(row=len(label_dictionary) - 1, column=1, sticky=tk.W, padx=20)
 type_list.grid(row=1)
-
 
 def vartrace(name, indx, op):
   fullist = set(possibilities.keys())
@@ -51,27 +53,35 @@ def vartrace(name, indx, op):
     if compstr[i] == "0": continue
     complist = fullist.copy()
     for g in complist:
-      if not compstr[i] == possibilities[g][i]:
+      if not compstr[i] == possibilities[g][0][i]: # compares place I of string with matching key in possibilities, first element of list, place I in string.
         fullist.remove(g)
   # Now update the display list
+  row = 0
   for k, v in label_dictionary.items():
-    if k in fullist: v.grid()
-    else: v.grid_forget()
+    if k in fullist:
+      # Text needs pad, so can't do for loop like lower down.
+      v[0].grid(row=row, column=0, sticky=tk.W)
+      v[1].grid(row=row, column=1, sticky=tk.W, padx=20)
+    else:
+      for i in v:
+        i.grid_forget()
+    row += 1
 
 chkvars = []
 for k, v in display_texts.items():
   newvar = tk.IntVar()
   newvar.trace("w", vartrace)
   chk = tk.Checkbutton(signs, text=k, justify=tk.LEFT, variable=newvar)
-  chk.grid(row=len(chkvars), column=0)
+  chk.grid(row=len(chkvars), column=0, sticky=tk.W)
   txt = tk.Label(signs, text=v)
-  txt.grid(row=len(chkvars), column=1)
+  txt.grid(row=len(chkvars), column=1, sticky=tk.E)
   chkvars.append(newvar)
 
 # Changing amounts of possibilities causes window size changes. Not the nicest things.
 rootwin.resizable(tk.FALSE, tk.FALSE)
 rootwin.update()
 
+type_list.grid_propagate(False)
 rootwin.grid_propagate(False)
 
 rootwin.mainloop()
